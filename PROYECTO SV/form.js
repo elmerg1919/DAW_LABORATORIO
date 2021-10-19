9338b7a59db6db07aa9d4e371881d32f2295356d
CristianAdonay <clashroyaleclashofclansclyx@gmail.com>
2021-10-18 19:54:39 -0600

    Modificacion de archivos
    Modifiqué los archivos form.html, form.js y style_Eli.css

--- PROYECTO SV/form.js
+++ PROYECTO SV/form.js
@@ -79,3 +79,131 @@ expconsulta = /^[A-z\s\S]{0,150}$/
 
 
 }
+
+var myArrayObject = [];
+var divState;
+function init() {
+    console.log("Carga del formulario realizada.");
+    if(addEventListener){
+        document.getElementById('agregar').addEventListener('click', save, false);
+        document.getElementById('obtener').addEventListener('click', obtain, false);
+        document.getElementById('borrar').addEventListener('click', remove, false);
+        document.getElementById('limpiar').addEventListener('click', clearStorage, false);
+
+        document.getElementById('guardar').addEventListener('click', saveComplexData, false);       
+        document.getElementById('gArreglo').addEventListener('click', saveArrayData, false);
+    }
+
+    if(typeof(Storage) == "undefined") {
+        alert("Este navegador no soporta HTML5 y almacenamiento local. Por favor, se recomienda actualizarlo");
+    }
+    else {
+        console.log("Este navegador soporta HTML5 y almacenamiento local como LocalStorage y sessionStorage.");
+        divState = document.getElementById("estado");
+    }
+    divState = document.getElementById("estado");
+    if (typeof(localStorage) == "undefined") {
+        divState.style.display = 'none';
+    }
+    else {
+        divState.style.display = 'block';
+    }
+}
+function save() {
+    var nombre = document.getElementById("name");
+    var correo = document.getElementById("gmail");
+    var pais = document.getElementById("pais");
+    var consulta = document.getElementById("consulta");
+    var msj;
+
+    try {
+        localStorage.setItem("name", nombre.value);
+        localStorage.setItem("gmail", correo.value);
+        localStorage.setItem("pais", pais.value);
+        localStorage.setItem("consulta", consulta.value);
+        nombre.value = "";
+        correo.value = "";
+        pais.value = "";
+        consulta.value = "";
+        msj = "Datos guardados con éxito en el localStorage.";
+        console.log(msj);
+        divState.innerHTML = "<p>" + msj + "</p>";
+
+    } catch(e) {
+        if (e >= QUOTA_EXCEEDED_ERR) {
+            console.log("Error: Se ha alcanzado el límite de almacenamiento local.")
+        }
+        else {
+            console.log("Error: Guardando datos con éxito en el almacenamiento local.");
+        }
+    }
+}
+function obtain() {
+    var msj = " Recibiendo el dato " + localStorage.key(0) + ", " +
+    localStorage.key(1) + ", " + localStorage.key(2) + " y " + localStorage.key(3) + " desde el localStorage.";
+    var nombre = document.getElementById("name");
+    var correo = document.getElementById("gmail");
+    var pais = document.getElementById("pais");
+    var consulta = document.getElementById("consulta");
+    console.log(msj);
+    divState.innerHTML = "<p>" + msj + "</p>";
+    nombre.value = localStorage.getItem("name");
+    correo.value = localStorage.getItem("gmail");
+    pais.value = localStorage.getItem("pais");
+    consulta.value = localStorage.getItem("consulta");
+}
+function remove() {
+    var msj = "Removiendo el dato del localStorage.";
+    console.log(msj);
+    divState.innerHTML = "<p>" + msj + "</p>";
+    localStorage.removeItem("name");
+    localStorage.removeItem("gmail");
+    localStorage.removeItem("pais");
+    localStorage.removeItem("consulta");
+}
+function clearStorage() {
+    var msj = "Borrando todos los datos del localStorage.";
+    console.log(msj);
+    divState.innerHTML = "<p>" + msj + "</p>";
+    localStorage.clear();
+}
+function saveComplexData() {
+    var msj = "Guardando el dato complejo en el localStorage.";
+    console.log(msj);
+    divState.innerHTML = "<p>" + msj + "</p>";
+    var nombre = document.getElementById("name");
+    var correo = document.getElementById("gmail");
+    var pais = document.getElementById("pais");
+    var consulta = document.getElementById("consulta");
+    var personaObject = new Object();
+    personaObject.name = nombre.value;
+    personaObject.correo = correo.value;
+    personaObject.pais = pais.value;
+    personaObject.consulta = consulta.value;
+    localStorage.setItem("persona", JSON.stringify(personaObject));
+}
+function saveArrayData() {
+    var msj = "Guardando el arreglo en el localStorage.";
+    var nombre = document.getElementById("name");
+    var correo = document.getElementById("gmail");
+    var pais = document.getElementById("pais");
+    var consulta = document.getElementById("consulta");
+    var personaObject1 = new Object();
+    personaObject1.name = nombre.value;
+    personaObject1.correo = correo.value;
+    personaObject1.pais = pais.value;
+    personaObject1.consulta = consulta.value;
+    myArrayObject.push(personaObject1);
+    console.log(msj);
+    divState.innerHTML = "<p>" + msj + "</p>";
+    localStorage.setItem("personas", JSON.stringify(myArrayObject));
+}
+
+
+
+if (window.addEventListener) {
+    window.addEventListener("load", init, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent("onload", init);
+}
